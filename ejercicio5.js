82c05b96aa250553cf0cfe5ba4194eadcd33b3c1
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-07 11:42:03 -0500

    Ejercicio 5 terminado

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,6 +1,10 @@
 let usuarios = [{nombre: "Juan", movimientos: [250000, -50000, 30000]},
 {nombre: "Ana", movimientos: [100000, -25000]}, {nombre: "Pedro", movimientos: [50000, 20000, -10000]}];
-for(let i; i < usuarios.length; i++){
+for(let i = 0; i < usuarios.length; i++){
     let totalUsuario = 0
-    for(let j; j < movimientos.length; j++){}
+    for(let j = 0; j < usuarios[i].movimientos.length; j++){
+        totalUsuario = totalUsuario + usuarios[i].movimientos[j];
+    }
+    console.log(usuarios[i].nombre);
+    console.log(totalUsuario);
 }

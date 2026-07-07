9f899aa5f368d4e90961ed97af7b46ea84dfc9a6
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-07 10:09:32 -0500

    respuestas

--- ejercicio1.js
+++ ejercicio1.js
@@ -8,7 +8,16 @@ for(let i = 0; i <movimientos.length; i++){
     if(movimientos[i] < 0){ //movimientos que están en la posición de [i]
         cantidadRetiros = cantidadRetiros + 1;
     }
+
+    console.log("Total: " + total);
 }
 
 console.log("Total: " + total);
-console.log("Cantidad de Retiros" + cantidadRetiros);
+console.log("Cantidad de Retiros: " + cantidadRetiros);
+
+//¿Que pasa si pones el console.log del total dentro del bucle en vez de afuera? 
+// imprimira en pantalla la suma acumulada de cada posición del array
+//¿Cuantas veces se imprime?
+//Se imprime las veces que tenga el tamaño del array, es decir 6 veces en este caso
+//¿Cuál es el valor "bueno", el ultimo o los del camino?
+//eEl ultimo

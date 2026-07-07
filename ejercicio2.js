4cbbd50f6f07d86dc470733735eb8aa5fc1a8f52
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-07 10:29:23 -0500

    Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+const pinCorrento = 4580;
+let intento = Number (prompt("Ingresa tu pin:   "));
+let numeroIntentos = 0;
+
+while(intento != pinCorrento){
+    numeroIntentos++;
+    console.log("Pin incorrecto");
+    console.log("Intentos: " + numeroIntentos);
+
+    intento = Number (prompt("Ingresa tu pin:   "));
+}
+console.log("Pin correcto")
